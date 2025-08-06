7aad5aa640a410095e85278ec05ec0d395f69f77
ketto26 <86974154+ketto26@users.noreply.github.com>
2025-08-06 17:06:41 +0400

    Refactor: Fixed DispatchGroupSequentialExecution task

--- InfoStorage/DispatchGroupSequentialExecution.playground/Contents.swift
+++ InfoStorage/DispatchGroupSequentialExecution.playground/Contents.swift
@@ -1,5 +1,5 @@
 import Foundation
-
+import PlaygroundSupport
 
 func executeTask(_ taskNumber: Int, delay: UInt32) {
     print("Task \(taskNumber) started")
@@ -8,35 +8,30 @@ func executeTask(_ taskNumber: Int, delay: UInt32) {
 }
 
 func executeTasks() {
-
+    
     let queue1 = DispatchQueue(label: "com.example.queue1", attributes: .concurrent)
     let queue2 = DispatchQueue(label: "com.example.queue2", attributes: .concurrent)
     let queueFinal = DispatchQueue(label: "com.example.finalQueue")
     let dispatchGroup = DispatchGroup()
-
-
-    dispatchGroup.enter()
-    queue1.async {
+    
+    queue1.async(group: dispatchGroup) {
         executeTask(1, delay: 2)
-        dispatchGroup.leave()
     }
-
-    dispatchGroup.enter()
-    queue2.async {
+    
+    queue2.async(group: dispatchGroup) {
         executeTask(2, delay: 3)
-        dispatchGroup.leave()
     }
-
+    
     dispatchGroup.notify(queue: queueFinal) {
         executeTask(3, delay: 1)
         print("All tasks sequence completed.")
+        PlaygroundSupport.PlaygroundPage.current.finishExecution()
     }
-
+    
     print("Initial tasks dispatched. Waiting for them to finish before Task 3.")
 }
 
 executeTasks()
 
-import PlaygroundSupport
 PlaygroundSupport.PlaygroundPage.current.needsIndefiniteExecution = true
 
